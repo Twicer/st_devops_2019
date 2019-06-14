0c70d7b183559d532732c3b2c762b8d72a05ac45
iamkalashnikov <kalashnikovgomel@gmail.com>
2019-06-14 10:23:08 +0300

    Module2 Task6 is ready to check

--- ST_DevOps2_2019_Nikita_Kalashnikov_Module2_Task6.ps1
+++ ST_DevOps2_2019_Nikita_Kalashnikov_Module2_Task6.ps1
@@ -36,7 +36,7 @@ Param (
     [string]$FirstAddress,
     [parameter(Mandatory=$true,HelpMessage="Enter Second IP Address")]
     [string]$SecondAddress,
-    [parameter(Mandatory=$true,HelpMessage="Enter Subnet CIDR Mask")]
+    [parameter(Mandatory=$true,HelpMessage="Enter Subnet CIDR Mask (example:'/25')")]
     [string]$SubnetMask
 )
 
@@ -47,24 +47,103 @@ $IPNetworkOne = (Get-IPv4Details -IPAddress $FirstAddress -SubnetMask $TestSubne
 $IPNetworkTwo = (Get-IPv4Details -IPAddress $SecondAddress -SubnetMask $TestSubnetMask).NetDottedDecimal
 
 if ($IPNetworkOne -eq $IPNetworkTwo) {
-    "IP addresses $FirstAddress and $SecondAddress  IS in the same  subnet mask $SubnetMask"
+    "IP addresses $FirstAddress and $SecondAddress are in the same subnet mask $SubnetMask"
 } else {
-    "Different Subnets"
+    "Different Subnets!"
 }
+
+<# Output:
+
+PS C:\WINDOWS\system32> C:\mask.ps1 -FirstAddress 10.11.12.13 -SecondAddress 10.121.17.14 -SubnetMask /24
+Different Subnets!
+
+PS C:\WINDOWS\system32> C:\mask.ps1 -FirstAddress 10.11.12.13 -SecondAddress 10.121.17.14 -SubnetMask /8
+IP addresses 10.11.12.13 and 10.121.17.14  IS in the same  subnet mask /8
+
+#>
 
 #2.	Работа с Hyper-V
 #2.1.	Получить список коммандлетов работы с Hyper-V (Module Hyper-V)
-#2.2.	Получить список виртуальных машин
-    Get-VM 
+Get-Command -Module Hyper-V
+
 <# Output:
+
+    CommandType     Name                                               Version    Source
+-----------     ----                                               -------    ------
+Cmdlet          Add-VMAssignableDevice                             2.0.0.0    Hyper-V
+Cmdlet          Add-VMDvdDrive                                     2.0.0.0    Hyper-V
+Cmdlet          Add-VMFibreChannelHba                              2.0.0.0    Hyper-V
+Cmdlet          Add-VMGpuPartitionAdapter                          2.0.0.0    Hyper-V
+Cmdlet          Add-VMGroupMember                                  2.0.0.0    Hyper-V
+Cmdlet          Add-VMHardDiskDrive                                2.0.0.0    Hyper-V
+Cmdlet          Add-VMHostAssignableDevice                         2.0.0.0    Hyper-V
+Cmdlet          Add-VMKeyStorageDrive                              2.0.0.0    Hyper-V
+Cmdlet          Add-VMMigrationNetwork                             2.0.0.0    Hyper-V
+Cmdlet          Add-VMNetworkAdapter                               2.0.0.0    Hyper-V
+Cmdlet          Add-VMNetworkAdapterAcl                            2.0.0.0    Hyper-V
+Cmdlet          Add-VMNetworkAdapterExtendedAcl                    2.0.0.0    Hyper-V
+Cmdlet          Add-VMNetworkAdapterRoutingDomainMapping           2.0.0.0    Hyper-V
+Cmdlet          Add-VMPmemController                               2.0.0.0    Hyper-V
+Cmdlet          Add-VMRemoteFx3dVideoAdapter                       2.0.0.0    Hyper-V
+Cmdlet          Add-VMScsiController                               2.0.0.0    Hyper-V
+Cmdlet          Add-VMStoragePath                                  2.0.0.0    Hyper-V
+Cmdlet          Add-VMSwitch                                       2.0.0.0    Hyper-V
+Cmdlet          Add-VMSwitchExtensionPortFeature                   2.0.0.0    Hyper-V
+Cmdlet          Add-VMSwitchExtensionSwitchFeature                 2.0.0.0    Hyper-V
+Cmdlet          Add-VMSwitchTeamMember                             2.0.0.0    Hyper-V
+Cmdlet          Checkpoint-VM                                      2.0.0.0    Hyper-V
+Cmdlet          Compare-VM                                         2.0.0.0    Hyper-V
+.....................................................................................
+Cmdlet          Wait-VM                                            2.0.0.0    Hyper-V
+#>
+
+#2.2.	Получить список виртуальных машин
+Get-VM 
+
+<# Output:
+
     Name            State  CPUUsage(%) MemoryAssigned(M) Uptime           Status             Version
 ----            -----  ----------- ----------------- ------           ------             -------
 KALASHNIKOV_VM1 Paused 0           2048              00:00:45.9100000 Operating normally 8.3
 KALASHNIKOV_VM2 Off    0           0                 00:00:00         Operating normally 8.3
 
 #>
+
 #2.3.	Получить состояние имеющихся виртуальных машин
+get-vm | Select-Object -Property Name, State
+
+<# Output:
+
+    Name             State
+----             -----
+KALASHNIKOV_VM1 Paused
+KALASHNIKOV_VM2    Off
+
+#>
+
 #2.4.	Выключить виртуальную машину
+Stop-VM -Name kalashnikov_vm1
 #2.5.	Создать новую виртуальную машину
+New-VM -Name kalashnikov_vm -NewVHDSizeBytes 20Gb -NewVHDPath `
+'D:\Virtual Machines\Hyper-V\kalashnikov_vm\Virtual Hard Disks\kalashnikov_vm.vhdx' `
+-Path 'D:\Virtual Machines\Hyper-V\kalashnikov_vm\' -MemoryStartupBytes 1024MB
+
+# Ставим число процессоров 2
+Set-VMProcessor -VMName kalashnikov_vm -Count 2
+
+# Добавляем ранее созданный Private адаптер KALASHNIKOV_LAN
+Connect-VMNetworkAdapter -VMName kalashnikov_vm -SwitchName KALASHNIKOV_LAN
+
+<# Output:
+
+Name           State CPUUsage(%) MemoryAssigned(M) Uptime   Status             Version
+----           ----- ----------- ----------------- ------   ------             -------
+kalashnikov_vm Off   0           0                 00:00:00 Operating normally 8.3
+
+#>
+
 #2.6.	Создать динамический жесткий диск
+New-VHD -Dynamic -SizeBytes 10Gb -Path 'D:\Virtual Machines\Hyper-V\kalashnikov_vm\Virtual Hard Disks\backup_kalashnikov_vm.vhdx'
+
 #2.7.	Удалить созданную виртуальную машину
+Remove-VM -VMName  kalashnikov_vm
